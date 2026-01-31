5bd4eee4716f4cfd9507aa09499c59346799e3f1
LapochnihIvan <lapochnihivan@gmail.com>
2026-01-31 13:27:04 +0300

    [+] Add macros for archived emdebed files

--- components/utils/include/utils/embed_file.h
+++ components/utils/include/utils/embed_file.h
@@ -11,12 +11,19 @@
         asm("_binary_" #name "_" #format "_start");                     \
     extern const uint8_t embed_file_ ## name ## _ ## format ## _end[]   \
         asm("_binary_" #name "_" #format "_end")
+
+#define EXTERN_EMBED_ARCHIVED_FILE(name, format, archive_format) \
+    EXTERN_EMBED_FILE(name, format ## _ ## archive_format)
+
 #define GET_EMBED_FILE(name, format)                           \
 {                                                              \
     .begin = embed_file_ ## name ## _ ## format ## _begin,     \
     .len = (size_t)(embed_file_ ## name ## _ ## format ## _end \
         - embed_file_ ## name ## _ ## format ## _begin)        \
 }
+
+#define GET_EMBED_ARCHIVED_FILE(name, format, archive_format) \
+    GET_EMBED_FILE(name, format ## _ ## archive_format)
 
 typedef struct embed_file
 {
