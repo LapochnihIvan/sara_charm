2ee061a6c2da7c6111b1784204ee6fafeab0f4fe
LapochnihIvan <lapochnihivan@gmail.com>
2026-02-13 15:05:58 +0300

    [+] Add function for Wi-Fi reconfiguration

--- components/wifi_point/include/wifi_point.h
+++ components/wifi_point/include/wifi_point.h
@@ -2,8 +2,17 @@
 #define SARA_CHARM_SERVER_WIFI_POINT_H
 
 
+#include <stdint.h>
+
+#include <esp_err.h>
+
+
 void start_wifi_point(void);
 void stop_wifi_point(void);
+esp_err_t change_wifi_point_settings(const char* ssid,
+                                     uint8_t ssid_len,
+                                     const char* password,
+                                     uint8_t password_len);
 
 
 #endif //!SARA_CHARM_SERVER_WIFI_POINT_H
--- components/wifi_point/src/wifi_point.c
+++ components/wifi_point/src/wifi_point.c
@@ -1,5 +1,7 @@
 #include "wifi_point.h"
 
+#include <string.h>
+
 #include <esp_wifi.h>
 
 
@@ -13,6 +15,8 @@ void start_wifi_point(void)
 
     wifi_init_config_t init_config = WIFI_INIT_CONFIG_DEFAULT();
     esp_wifi_init(&init_config);
+    esp_wifi_set_mode(WIFI_MODE_AP);
+    esp_wifi_set_storage(WIFI_STORAGE_RAM);
 
     wifi_config_t config = {
         .ap = {
@@ -31,9 +35,26 @@ void start_wifi_point(void)
         },
     };
 
-    esp_wifi_set_mode(WIFI_MODE_AP);
     esp_wifi_set_config(WIFI_IF_AP, &config);
     esp_wifi_start();
+}
+
+esp_err_t change_wifi_point_settings(const char* const ssid,
+                                     const uint8_t ssid_len,
+                                     const char* const password,
+                                     const uint8_t password_len)
+{
+    wifi_config_t wifi_config;
+    (void)esp_wifi_get_config(ESP_IF_WIFI_AP, &wifi_config);
+
+    memcpy((void*)wifi_config.ap.ssid, (const void*)ssid, ssid_len);
+    wifi_config.ap.ssid_len = ssid_len;
+
+    memcpy((void*)wifi_config.ap.password, (const void*)password, password_len);
+    wifi_config.ap.password[password_len] = '\0';
+
+    (void)esp_wifi_deauth_sta(0);
+    return esp_wifi_set_config(ESP_IF_WIFI_AP, &wifi_config);
 }
 
 void stop_wifi_point(void)
