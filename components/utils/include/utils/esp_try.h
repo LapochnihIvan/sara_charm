ca578b7ca84a1869a4a9946f95560dad87e4b19e
LapochnihIvan <lapochnihivan@gmail.com>
2026-02-14 02:14:16 +0300

    [r] Change formating

--- components/utils/include/utils/esp_try.h
+++ components/utils/include/utils/esp_try.h
@@ -7,14 +7,14 @@
 #include <esp_err.h>
 
 
-#define ESP_TRY(expr)                     \
-do {                                      \
-    const esp_err_t esp_try_res = (expr); \
-    if (esp_try_res != ESP_OK)            \
-    {                                     \
-        return esp_try_res;               \
-    }                                     \
-} while (false)
+#define ESP_TRY(expr)                         \
+    do {                                      \
+        const esp_err_t esp_try_res = (expr); \
+        if (esp_try_res != ESP_OK)            \
+        {                                     \
+            return esp_try_res;               \
+        }                                     \
+    } while (false)
 
 
 #endif //!SARA_CHARM_UTILS_ESP_TRY_H
