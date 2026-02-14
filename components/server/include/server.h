f843fe04a467f04db018b0820076ffc3d4439e01
LapochnihIvan <lapochnihivan@gmail.com>
2026-02-14 15:21:27 +0300

    [r] Add missing argument name

--- components/server/include/server.h
+++ components/server/include/server.h
@@ -4,11 +4,13 @@
 
 #include <esp_http_server.h>
 
+#include "wifi_point.h"
+
 
 typedef httpd_handle_t server_handle_t;
 
 esp_err_t start_server(server_handle_t server);
-void stop_server(server_handle_t);
+void stop_server(server_handle_t server);
 
 
 #endif //!SARA_CHARM_SERVER_SERVER_H
