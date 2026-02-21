31f51db6b13a60a03b19fd704846c0752b871441
LapochnihIvan <lapochnihivan@gmail.com>
2026-02-21 21:40:41 +0300

    [+] Add footer on front

--- frontend/src/components/app.rs
+++ frontend/src/components/app.rs
@@ -2,9 +2,21 @@ use yew::prelude::*;
 
 use super::settings::Settings;
 
+use crate::utils::get_css::get_css_global;
+
+const SOURCE_CODE_LINK: &str =
+    "https://github.com/LapochnihIvan/sara_charm.git";
+
 #[function_component(App)]
 pub fn app() -> Html {
+    get_css_global!("app.css");
+
     html! {
-        <Settings />
+        <>
+            <Settings />
+            <footer>
+                <a href={SOURCE_CODE_LINK} target="_blank">{"Исходный код"}</a>
+            </footer>
+        </>
     }
 }
